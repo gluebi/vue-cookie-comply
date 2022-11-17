e31aeb15701d522acc7c49a31e12b1c81646bcd9
Oliver Pietsch <kontakt@oliver-pietsch.de>
2022-11-17 15:58:39 +0100

    #36 Vue 2 compat

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import CookieComply from './components/CookieComply.vue';
+import { isVue2, Vue2 } from 'vue-demi'
 
 export default {
   install: (app) => {
     if (!window) return;
 
-    app.component('VueCookieComply', CookieComply);
+    if (isVue2) {
+      // Vue 2 only
+      Vue2.component(CookieComply)
+    } else {
+      // Vue 3 only
+      app.component('VueCookieComply', CookieComply);
+    }
   },
 };
